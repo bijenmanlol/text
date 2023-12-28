ac8568641e0562ef4bca39777980b72332973450
bijenmanlol <noah.achtergaele@gmail.com>
2023-12-28 03:04:30 +0100

    Implemented locally saving of color scheme

--- example/script.js
+++ example/script.js
@@ -1,15 +1,27 @@
-let colorScheme = "light";
+if(localStorage.getItem("color-scheme") == null) {
+    localStorage.setItem("color-scheme", "light");
+}
 
-document.getElementById("light").style.display = "none";
+if(localStorage.getItem("color-scheme") == "dark"){
+    localStorage.setItem("color-scheme", "dark");
+    document.getElementsByTagName("html")[0].setAttribute("current-color-scheme", "dark");
+    document.getElementById("light").style.display = "inline";
+    document.getElementById("dark").style.display = "none";
+}else{
+    localStorage.setItem("color-scheme", "light");
+    document.getElementsByTagName("html")[0].setAttribute("current-color-scheme", "light");;
+    document.getElementById("dark").style.display = "inline";
+    document.getElementById("light").style.display = "none";
+}
 
 document.getElementById("color-scheme-selector").addEventListener("click", function() {
-    if(colorScheme == "dark"){
-        colorScheme = "light";
+    if(localStorage.getItem("color-scheme") == "dark"){
+        localStorage.setItem("color-scheme", "light");
         document.getElementsByTagName("html")[0].setAttribute("current-color-scheme", "light");
         document.getElementById("dark").style.display = "inline";
         document.getElementById("light").style.display = "none";
     }else{
-        colorScheme = "dark";
+        localStorage.setItem("color-scheme", "dark");
         document.getElementsByTagName("html")[0].setAttribute("current-color-scheme", "dark");;
         document.getElementById("light").style.display = "inline";
         document.getElementById("dark").style.display = "none";
